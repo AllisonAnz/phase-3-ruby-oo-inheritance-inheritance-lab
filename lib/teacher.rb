955e66f523b0103b945d257e7cf3b87a1063d8f9
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-29 13:44:06 -0500

    commit tree 0e002c66e76848a3c7d853373117a911f0af8204

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,11 @@
-class Teacher
+# Change the class definition so that the Teacher Inherits from User class
+
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  # a method that returns a random element from that array 
+  def teach 
+    KNOWLEDGE.sample 
+  end
 end
---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+# Define User class such that a user can find a first and last name 
+# You'll need a setter and getter for first and last name 
+
+class User 
+    attr_accessor :first_name, :last_name
+end
