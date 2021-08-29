26802bcbec9cae7eda3d47a85a0d366ed91c8baf
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-29 13:44:21 -0500

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+# Change the class definition so that it inherits from the User class
+class Student < User
+
+    attr_reader :knowledge
+
+    # Individual students should initialize with an instance variable @knowledge that points to an empty array
+    def initialize
+        @knowledge = []
+    end
+
+    # A method that takes in a string and adds that string to the student's knowledge array
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge 
+        @knowledge
+    end
 
 end
---
+++ testfile.rb
@@ -0,0 +1,3 @@
+require_relative "../lib/user"
+require_relative "../lib/teacher"
+require_relative "../lib/student"
